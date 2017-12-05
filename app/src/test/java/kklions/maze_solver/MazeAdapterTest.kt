0b66858129bdfde9581518330099ec952cd787d2
KLIOK002 <Kevin.H.Klions@disney.com>
2017-12-05 15:41:57 -0800

    configured for kotlin, added kotlin unit test stub

---
+++ app/src/test/java/kklions/maze_solver/MazeAdapterTest.kt
@@ -0,0 +1,19 @@
+package kklions.maze_solver
+
+import kklions.mazesolver.model.MazeConfiguration
+import org.junit.Test
+
+/**
+ *  Unit Test written in kotlin
+ *
+ * Created by kliok002 on 12/5/17.
+ */
+
+class MazeAdapterTest {
+
+    @Test
+    fun testConfiguration() {
+
+    }
+
+}
